ace923c814f4ec4d89ea205eaa0df25e52649be3
jql <jql@jql-pc.(none)>
2016-01-18 03:20:58 -0500

    ready to use

--- define.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GLOBAL_H_
-#define __GLOBAL_H_
-
-#define MAX_PATH_SIZE	256
-#define LINE_MAX    1024
-#define IPADDR_SIZE 16
-#define PROCESSPOOL_ERROR_LOG_FILENAME      "processpool"
-#endif
